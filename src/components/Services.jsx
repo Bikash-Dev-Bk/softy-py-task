3f0dc98ab6916c090630ca93aead6c49e12cb41e
satya_ranjon <bikash.dev.bk@gmail.com>
2024-05-09 16:56:03 +0600

    service title added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,16 @@
-
-
 const Services = () => {
-    return (
-        <div>
-            <p>This is Services</p>
-        </div>
-    );
+  return (
+    <div className="bg-[#159EEC03] py-20">
+      <div className="text-center">
+        <p className="uppercase text-[#159EEC] font-bold tracking-[.12em]">
+          Care you can believe in
+        </p>
+        <h2 className="text-[#1F2B6C] text-2xl lg:text-3xl font-bold mb-3 mt-1">
+          Our Services
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
